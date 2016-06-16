91d10a6706ce4788ddd200cc5b8963305bd24fb1
glennarcher-nz <glennatcher@gmail.com>
2016-06-16 20:14:51 +1200

    TM4 - Repository
    Repository for TM4

---
+++ receiveform.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
+
+"http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+
+<html>
+
+<head>
+
+<title>Membership PHP Test Form</title>
+
+<meta http-equiv="content-type" content="text/html; charset=iso-8859-1" />
+
+</head>
+
+<body>
+
+<?php
+
+$MembershipID = $_POST['MembershipID']
+$FirstName = $_POST['FirstName']
+$FamilyName = $_POST['FamilyName']
+$EmailAddress = $_POST['EmailAddress']
+$PhoneNumber = $_POST['Phone']
+
+?>
+
+</body>
+
+</html>
